6adfb2ff06b4a8e32b5b980c81cba0b5adf79b0a
MariaRafailova <mrafailova@gmail.com>
2017-01-29 22:58:41 +0200

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HeaderFooter(n);
+
+            for (int i = 0; i < n - 2; i++)
+                MiddleRow(n);
+
+            HeaderFooter(n);
+        }
+
+        static void HeaderFooter(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+        }
+
+        static void MiddleRow(int n)
+        {
+            Console.Write('-');
+
+            for (int i = 1; i<n; i++)
+            {
+                Console.Write(@"\/");
+            }
+
+            Console.WriteLine('-');
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintTriangle(int n)
+        {
+            for (int line = 0; line < n; line++)
+                PrintLine(1, line);
+
+            PrintLine(1, n);
+
+            for (int line = n - 1; line >= 0; line--)
+                PrintLine(1, line);
+        }
+    }
+}
